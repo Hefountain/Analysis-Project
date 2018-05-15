a9768aecbe4d90f21a961b46876892115604c0ad
fountainHe <abc200868@126.com>
2018-05-15 18:35:15 +0800

    up
    up

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -10,15 +10,15 @@ import scrapy
 
 class TutorialItem(scrapy.Item):
 # define the fields for your item here like:
-# 用户名
+	# 用户名
 	user_name = scrapy.Field()
-	# 评论时
+	# 评论时间
 	comment_time = scrapy.Field()
 	# 评分
 	score = scrapy.Field()
 	# 认为有用的人数
 	agree_num = scrapy.Field()
-	# 影
+	# 影评
 	comment = scrapy.Field()
 	# 是否看过
 	is_view = scrapy.Field()
